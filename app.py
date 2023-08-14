1b51c9ae6229971fefbe9c26034641f4c50ba3de
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-08-13 18:04:50 -0700

    update for class example

--- app.py
+++ app.py
@@ -7,18 +7,19 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 openai.api_key = OPENAI_API_KEY
 
-prompt = "Tell me a joke"
+prompt = "Tell me a story about a frog"
 
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
     {"role": "system", 
-     "content": "You are a helpfu assistant"},
+     "content": "You are a story teller"},
     {"role": "user", 
      "content": prompt}
   ],
   temperature=0,
-  top_p=.1
+  top_p=0,
+  max_tokens=50
 )
 
 print(completion.choices[0].message.content)
