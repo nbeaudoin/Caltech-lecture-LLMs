7a83904f2a21898d4034a0d33ad9013c8b2e6b76
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-08-15 15:10:02 -0700

    Created simple app for class

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
 import os
 from dotenv import load_dotenv
-import openai
+from langchain.llms import OpenAI
+import streamlit as st
 
 # Load credentuals
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-openai.api_key = OPENAI_API_KEY
+OpenAI.api_key = OPENAI_API_KEY
 
-prompt = "Tell me a story about a frog"
+st.title('LLM Model')
+prompt = st.text_input('Enter a prompt here')
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", 
-     "content": "You are a story teller"},
-    {"role": "user", 
-     "content": prompt}
-  ],
-  temperature=0,
-  top_p=1,
-  max_tokens=100
-)
+llm = OpenAI(temperature=2,
+             max_tokens=100,
+             )
 
-print(completion.choices[0].message.content)
-
+if prompt:
+    response = llm(prompt)
+    st.write(response)
 
